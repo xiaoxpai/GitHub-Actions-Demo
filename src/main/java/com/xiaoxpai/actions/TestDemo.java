d731e242ceb54cb12fec562a18877018cd20eebd
F1686352 <hzlh-mis-xiao.lh@foxconn.com>
2023-04-17 16:11:55 +0800

    init project

---
+++ src/main/java/com/xiaoxpai/actions/TestDemo.java
@@ -0,0 +1,10 @@
+package com.xiaoxpai.actions;
+
+/**
+ * @author F1686352
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Github Actions.");
+    }
+}
